a372e83d40f7160666b9c1eae58b9ee3b5573b5a
tlsgptj <angele0709@naver.com>
2025-05-27 17:39:49 +0900

    ✨ Splash screen

---
+++ src/app/splash/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect } from "react";
+import Image from "next/image";
+import splash from "../../../public/images/splash.png";
+import splashName from "../../../public/images/splash_name.png";
+import { useRouter } from "next/navigation";
+
+export default function SplashPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <div className="relative w-screen h-screen">
+      <Image
+        src={splash}
+        alt="Splash"
+        fill
+        className="object-cover"
+        priority
+      />
+
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Image
+          src={splashName}
+          alt="Splash Name"
+          width={200}
+          height={100}
+          priority
+        />
+      </div>
+    </div>
+  );
+}
