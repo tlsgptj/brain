8d742161113daf43f774c80c90d2a835a2a2475d
tlsgptj <angele0709@naver.com>
2026-01-02 08:44:49 +0900

    💄 로고 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import Image from "next/image";
-import Logo from "../../../public/images/logo.png";
+import Logo from "../../../public/images/splash_name.png";
 import { useRouter } from 'next/navigation';
 import splash from "../../../public/images/splash.png";
 
